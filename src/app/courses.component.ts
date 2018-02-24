303cead1dac08340e5f3cb93e647823c88abb105
gaurav <gaurav.dighe1986@gmail.com>
2018-02-24 23:19:09 +0530

    Directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,11 +5,17 @@ import { Component } from "@angular/core";
     template: `
         <h1>{{title}}</h1>
         <h2>{{getTitle2()}}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{course}}
+            </li>
+        </ul>
         `
 })
 export class CoursesComponent {
     title = "Training Center";
     title2 = "Courses we offer";
+    courses = ["Java", "BigData", "Angular4", "ReactJS"];
 
     getTitle2() {
         return this.title2;
