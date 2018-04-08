accff5d82b11d0257386a05de6688e953ae60925
Gaurav-Laptop <gaurav.dighe1986@gmail.com>
2018-04-08 17:50:41 +0530

    Section 8 - Asyc Validators

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -7,4 +7,16 @@ export class UsernameValidators {
 
         return null;
     }
+
+    static shouldBeUnique(control: AbstractControl): Promise<ValidationErrors | null> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === "gaurav") {
+                    resolve({ shouldBeUnique: true })
+                } else {
+                    resolve(null);
+                }
+            }, 2000);
+        });
+    }
 }
