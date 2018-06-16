672f64c8fc836ba92cca11d82279fc4eb85301ea
Gaurav@Linux <gaurav.dighe1986@gmail.com>
2018-06-16 15:27:29 +0530

    Http GET

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { FavoriteComponent } from './favourite/favorite.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
@@ -11,6 +12,7 @@ import { SummaryPipe } from './summary.pipe';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { CoursesFormComponent } from './courses-form/courses-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
@@ -21,12 +23,15 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
         FavoriteComponent,
         ContactFormComponent,
         CoursesFormComponent,
-        SignupFormComponent
+        SignupFormComponent,
+        PostsComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        HttpModule
+
     ],
     providers: [
         CoursesService
