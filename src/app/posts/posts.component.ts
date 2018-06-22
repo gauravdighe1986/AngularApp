fdb72feb408124c77f1ed626c66c3c9829320aee
gaurav <gaurav.dighe1986@gmail.com>
2018-06-22 10:25:20 +0530

    Http POST

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,15 +7,32 @@ import { Http } from '@angular/http';
     styleUrls: ['./posts.component.css']
 })
 export class PostsComponent {
-
+    url = 'http://jsonplaceholder.typicode.com/posts';
     posts: any[];
 
-    constructor(http: Http) {
+    constructor(private http: Http) {
 
-        http.get('http://jsonplaceholder.typicode.com/posts')
+        http.get(this.url)
             .subscribe(response => {
                 this.posts = response.json();
             });
+    }
+
+    // Adds a post
+    createPost(input: HTMLInputElement) {
+        console.log('createPost() called: ' + input.value);
+        let post = { title: input.value };
+
+        this.http.post(this.url, JSON.stringify(post))
+            .subscribe(response => {
+                console.log(response.json());
+                post['id'] = response.json().id;
+                // this will add the post to the first position of the posts array
+                this.posts.splice(0, 0, post);
+            });
+
+        input.value = '';
+
     }
 
 
