9973e56d485f9e091b11c6b4cbfd52383eb5b8b4
gaurav <gaurav.dighe1986@gmail.com>
2018-06-23 09:47:04 +0530

    HTTP PUT and DELETE

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -11,13 +11,13 @@ export class PostsComponent implements OnInit {
     posts: any[];
 
     constructor(private http: Http) {
-
     }
 
     // Default behaviour will be written here. Here every times a page load http.get is called.
     ngOnInit() {
         this.http.get(this.url)
             .subscribe(response => {
+                console.log('HTTP STATUS: ' + response.status);
                 this.posts = response.json();
             });
     }
@@ -39,5 +39,22 @@ export class PostsComponent implements OnInit {
 
     }
 
+    updatePost(post) {
+        console.log('Post called');
+        this.http.put(this.url + '/' + post.id, JSON.stringify(post))
+            .subscribe(response => {
+                console.log('HTTP STATUS: ' + response.status);
+                console.log(response.json());
+            });
+    }
+
+    deletePost(post) {
+        console.log('Delete Post called');
+        this.http.delete(this.url + '/' + post.id, JSON.stringify(post))
+            .subscribe(response => {
+                console.log('HTTP STATUS: ' + response.status);
+                console.log(response.json())
+            });
+    }
 
 }
